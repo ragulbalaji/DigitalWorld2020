0a39fbfd63ae1de05dc62b182f9e87cebd9aeb44
Ragul Balaji <ragulbaby@gmail.com>
2020-04-10 21:06:12 +0800

    screen density accounted scales

--- FinalsGame/main.py
+++ FinalsGame/main.py
@@ -1,8 +1,10 @@
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+from kivy.core.audio import SoundLoader
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.widget import Widget
 from kivy.graphics import *
+from kivy.metrics import *
 
 from kivy.config import Config
 Config.set('graphics', 'resizable', False) # Fixed Size
@@ -10,12 +12,15 @@ Config.set('graphics', 'width', '800')
 Config.set('graphics', 'height', '600')
 Config.write()
 
+snd_hit = SoundLoader.load('assets/snd/Hit.wav')
+snd_pickup = SoundLoader.load('assets/snd/Pickup.wav')
+snd_shoot = SoundLoader.load('assets/snd/Shoot.wav')
+
 class GameCanvas(Widget):
 	def __init__(self, **kwargs):
 		super().__init__(**kwargs)
 		with self.canvas:
-			print("HELLO")
-			Rectangle(pos=(10,20),size=(100,200))
+			Rectangle(pos=(0,0),size=(dp(750),dp(550)))
 
 class MainMenu(Screen):
 	pass
@@ -27,6 +32,8 @@ layouts = Builder.load_file('layouts.kv')
 class GameApp(App):
 	title="Tanks! | Ragul Balaji 2020 | Digital World 10.009"
 	def build(self):
+		print(self.title)
+		snd_pickup.play()
 		return layouts
 
 gameapp = GameApp()
