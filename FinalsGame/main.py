b37273d0d6cebb8bb5ebc097fb75748de8e5c476
Ragul Balaji <ragulbaby@gmail.com>
2020-04-24 15:07:17 +0800

    dank game!

--- FinalsGame/main.py
+++ FinalsGame/main.py
@@ -30,6 +30,7 @@ Config.write()
 snd_hit = SoundLoader.load('assets/snd/Hit.wav')
 snd_pickup = SoundLoader.load('assets/snd/Pickup.wav')
 snd_shoot = SoundLoader.load('assets/snd/Shoot.wav')
+snd_explode = SoundLoader.load('assets/snd/Explode.wav')
 
 CLR_RED = (1,0,0,1)
 CLR_GRN = (0,1,0,1)
@@ -83,6 +84,51 @@ class myBox(Entity):
 			Color(*CLR_RED)
 			Rectangle(pos=self.pos, size=self.size)
 
+class HealthPack(Entity):
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
+		self.parentid = kwargs['parentid']
+		self.size = Vector(dp(50), dp(50))
+		self.lowerleftpos = self.pos - self.size/2
+	def die(self):
+		GAME_OBJS.remove(self)
+	def update(self, dt):
+		for obj in GAME_OBJS:
+			if obj.id != self.id and obj.id != self.parentid and hasattr(obj, 'hurt') and self.pos.distance(obj.pos) < dp(50) :
+				obj.hurt(-random.randint(40,60)) # heal
+				snd_pickup.play()
+				self.die()
+				return
+	def render(self, canvas):
+		with canvas:
+			PushMatrix()
+			Color(1,1,1,1)
+			Rectangle(pos=self.lowerleftpos, size=self.size, source='assets/img/health.png')
+			PopMatrix()
+
+class Explosion(Entity):
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
+		self.size = Vector(dp(10), dp(10))
+		maxsz = dp(random.randint(50, 200))
+		self.maxsize = Vector(maxsz, maxsz)
+		self.growthfactor = 1.5 + random.random()
+		snd_explode.play()
+	def die(self):
+		GAME_OBJS.remove(self)
+	def update(self, dt):
+		if self.size.x < self.maxsize.x:
+			self.size *= self.growthfactor
+		else:
+			self.die()
+			return
+	def render(self, canvas):
+		with canvas:
+			PushMatrix()
+			Color(1, 0.65, 0, 1)
+			Ellipse(pos=self.pos-self.size/2, size=self.size)
+			PopMatrix()
+
 class Bullet(Entity):
 	def __init__(self, **kwargs):
 		super().__init__(**kwargs)
@@ -102,8 +148,9 @@ class Bullet(Entity):
 			for obj in GAME_OBJS:
 				if obj.id != self.id and obj.id != self.parentid and self.pos.distance(obj.pos) < dp(max(obj.size)/4):
 					if hasattr(obj, 'hurt'):
-						obj.hurt(1)
+						obj.hurt(random.randint(1,3))
 						snd_hit.play()
+						GAME_OBJS.append(Explosion(pos=self.pos))
 					self.die()
 					return
 	def render(self, canvas):
@@ -116,7 +163,8 @@ class Bullet(Entity):
 class EnemyTank(Entity):
 	def __init__(self, **kwargs):
 		super().__init__(**kwargs)
-		self.health = 100
+		self.maxhealth = 10
+		self.health = self.maxhealth
 		self.speed = dp(100)
 		self.rotspd = 120
 		self.turretspd = 30
@@ -127,23 +175,29 @@ class EnemyTank(Entity):
 		self.capsize = Vector(dp(44), dp(44))
 		self.nextshottime = 0
 		self.reloadtime = 1.0 / 2
+	def die(self):
+		GAME_OBJS.remove(self)
+		GAME_OBJS.append(HealthPack(pos=self.pos, parentid=self.id))
 	def update(self, dt):
 		global PLAYER_OBJ
 
+		distancetoplayer = PLAYER_OBJ.pos.distance(self.pos)
 		self.playerheading = -Vector(1,0).angle(Vector(PLAYER_OBJ.pos)-self.pos)
 
-		self.turretangle += dt * self.turretspd * 1 if Vector(1,0).rotate(self.turretangle).angle(Vector(PLAYER_OBJ.pos)-self.pos) < 0 else -1
+		self.turretangle += dt * self.turretspd * (1 if Vector(1,0).rotate(self.turretangle).angle(Vector(PLAYER_OBJ.pos)-self.pos) < 0 else -1)
 
-		movedir = Vector(0, self.speed).rotate(self.rot)
-		if PLAYER_OBJ.pos.distance(self.pos) > 1: #max(self.size) * 6:
-			if abs(self.playerheading - self.rot) < 10:
-				self.velocity = movedir
+		if distancetoplayer > max(self.size) * 5.5 or distancetoplayer < max(self.size) * 3.5:
+			toberotated = Vector(1,0).rotate(self.playerheading).angle(Vector(0,1).rotate(self.rot))
+			self.rot += self.rotspd * dt * (1 if toberotated > 0 else -1)
+			if abs(toberotated) < 3: # pointed in the correct direction
+				movedir = Vector(0, self.speed).rotate(self.rot)
+				self.velocity = movedir * (1 if distancetoplayer > max(self.size) * 6 else -0.5)
 
 		if time.time() > self.nextshottime:
 			self.nextshottime = time.time() + self.reloadtime + random.random() * 2
 			bulletvel = Vector(dp(10),0).rotate(self.turretangle)
 			GAME_OBJS.append(Bullet(pos=self.pos+Vector(dp(60), 0).rotate(self.turretangle), velocity=bulletvel, parentid=self.id))
-			#self.velocity -= 10 * bulletvel # knockback
+			self.velocity -= 20 * bulletvel # knockback
 
 		self.pos += self.velocity * dt
 		self.velocity *= 0.9
@@ -166,12 +220,14 @@ class EnemyTank(Entity):
 			healthstr = str(self.health)
 			texLabel(healthstr, 25, (self.pos + Vector(-15*len(healthstr),-25)) / dp(1), CLR_WHI)
 	def hurt(self, dmg = 1):
-		self.health -= dmg
+		self.health = min(self.health - dmg, self.maxhealth)
+		if self.health <= 0: self.die()
 
 class PlayerTank(Entity):
 	def __init__(self, **kwargs):
 		super().__init__(**kwargs)
-		self.health = 100
+		self.maxhealth = 100
+		self.health = self.maxhealth
 		self.speed = dp(100)
 		self.rotspd = 120
 		self.turretspd = 60
@@ -202,6 +258,7 @@ class PlayerTank(Entity):
 			GAME_OBJS.append(Bullet(pos=self.pos+Vector(dp(60), 0).rotate(self.turretangle), velocity=bulletvel, parentid=self.id))
 			self.velocity -= 10 * bulletvel # knockback
 
+		if self.health > self.maxhealth: self.health -= 0.05 * dt * self.health
 		self.pos += self.velocity * dt
 		self.velocity *= 0.9
 		self.lowerleftpos = self.pos - self.size / 2
@@ -224,6 +281,9 @@ class PlayerTank(Entity):
 			Rectangle(pos=(dp(65),dp(10)), size=(dp(500),dp(50)))
 			Color(*CLR_RED) if self.health < 50 else Color(*CLR_GRN)
 			Rectangle(pos=(dp(65),dp(10)), size=(dp(5*self.health),dp(50)))
+			if self.health > self.maxhealth:
+				Color(*CLR_WHI)
+				texLabel('+'+str(int(self.health-self.maxhealth)), 45, (580, 10), CLR_WHI)
 			PopMatrix()
 	def hurt(self, dmg = 1):
 		self.health -= dmg
@@ -243,7 +303,7 @@ class GameCanvas(Widget):
 		PLAYER_OBJ = PlayerTank(pos=(dp(GAME_WIDTH/2), dp(GAME_HEIGHT/2)))
 		GAME_OBJS.append(PLAYER_OBJ)
 
-		for i in range(random.randint(2, 5)):
+		for i in range(random.randint(1, 3)):
 			GAME_OBJS.append(EnemyTank(pos=(dp(random.randint(0,GAME_WIDTH/2)), dp(random.randint(0,GAME_WIDTH/2)))))
 		
 		Clock.schedule_interval(self.tick, GAME_FPS)
